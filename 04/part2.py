1178a810367318368b5b5f0fbde57a4411d9c8e4
vilinden <111963449+vilinden@users.noreply.github.com>
2024-12-06 14:26:02 +0100

    Day 4

--- 04/part2.py
+++ 04/part2.py
@@ -1,34 +1,19 @@
-def RotateMatrix90(matrix):
-    """
-    Rotates matrix 90 degrees clockwise
-    """
-    rotatedMatrix = []
-
-    for col_index in range(len(matrix[0])):
-        column = [row[col_index] for row in matrix]
-        rotatedMatrix.append(column[::-1])
-    return rotatedMatrix
-
 def checkCross(matrix, i0, i1):
     found_matches = 0
-    if i0 >= len(matrix): return False
-    if i0+2 >= len(matrix) or i1+2 >= len(matrix[i0]):
-        return False
-    
-    MATCH_WORD = ['M', 'A', 'S']
-    firstCell = matrix[i0][i1]
-    if not firstCell in [MATCH_WORD[0], MATCH_WORD[-1]]:
+    MATCH_WORD = [['M', 'A', 'S'], ['S','A','M']]
+    directions = [
+        [(0, 0), (1, 1), (2, 2)],
+        [(0, 2), (1, 1), (2, 0)]
+    ]  # Diagonalriktning
+
+    if i0 + 2 >= len(matrix) or i1 + 2 >= len(matrix[0]):
         return False
 
-    for i in range(4):
-        if i > 0: matrix = RotateMatrix90(matrix)
-        test_word = [matrix[i0][i1], matrix[i0+1][i1+1], matrix[i0+2][i1+2]]
-        if test_word == MATCH_WORD: found_matches += 1
-    
-    if found_matches == 2:
-        return True
-    else:
-        return False
+    for i in range(2):  # Loop för att rotera koordinaterna
+        test_word = [matrix[i0 + dx][i1 + dy] for dx, dy in directions[i]]
+        if test_word in MATCH_WORD: found_matches += 1
+        if found_matches == 2: return True
+    return False
 
 test = [
     ['S','A','S'],
@@ -37,23 +22,19 @@ test = [
 ]
 
 letters = []
-sum = 0
 
 with open("04/input.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        line_list = []
-        for letter in line.strip():
-            line_list.append(letter)
-        letters.append(line_list)
+    letters = [list(line.strip()) for line in f]
 
 totalLetters = len(letters) * len(letters[0])
+sum_matches = 0
 counter = 1
 
-for i0, row in enumerate(letters):
-    for i1 in range(len(row)):
+for i0, row in enumerate(letters):  # Enumerate over rows
+    for i1 in range(len(row)):  # Iterate over columns
         print(f"Testing {counter} of {totalLetters}")
-        counter+=1
-        if checkCross(letters, i0, i1): sum += 1
+        if checkCross(letters, i0, i1):
+            sum_matches += 1
+        counter += 1  # Increment counter for each test
 
-print(f"Found {sum} correct crosses!")
+print(f"Found {sum_matches} correct crosses!")
